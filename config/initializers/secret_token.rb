e33443cf8891daced6a09c80ad6822e7cbe891a7
Tania Conca <tconca92@gmail.com>
2014-08-23 15:43:21 -0400

    configure tests

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 GymApp::Application.config.secret_key_base = '2b8cfe241b476a0f02cd474bd225a1343ff0ff03e83a2feabc71d6d00070e7cbd88d5833f376989e58555443042d75500da889575ddc7130a2b1b330aacdb33f'
