a0fffdc159524cec24738a7fcb51feaa9487ad12
Tania Conca <tconca92@gmail.com>
2014-08-23 11:45:11 -0400

    inicialización repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GymApp::Application.config.secret_key_base = '2b8cfe241b476a0f02cd474bd225a1343ff0ff03e83a2feabc71d6d00070e7cbd88d5833f376989e58555443042d75500da889575ddc7130a2b1b330aacdb33f'
